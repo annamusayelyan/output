d06320499539b3dd5623edf0c78ec344b3791092
Elina <elinahakobyan77@gmail.com>
2019-06-03 08:47:43 +0400

    Code split up

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,43 @@
 import 'package:flutter/material.dart';
-
+import 'package:first_app/products.dart';
 main() {
   runApp(MyApp());
 }
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
   build(context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(
-        title: Text('first name'),
-        backgroundColor: Colors.red,
-      ),
-      body: Column(
-        children:<Widget>[
-      RaisedButton(
-      child: Text('Press', style: TextStyle(color: Colors.black),),
-      color: Colors.red,
-      onPressed: () {},
-    ),
-          Card(
-            child: Column(children: <Widget>[
-              Image.asset('assets/food.jpg'),
-              Text('Delicious'),
-            ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('first app'),
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+        body: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text(
+                'Press',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              color: Colors.red,
+              onPressed: () {
+                setState(() {
+                  _products.add('Advanced food tester');
+                });
+              },
             ),
-
-          )
-    ]
-
-
-    )
-    ,
-    )
+           Products(_products)
+          ],
+        ),
+      ),
     );
   }
 }
